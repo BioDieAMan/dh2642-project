ae03cc2f23b31cb48def8818996f05e6c0b180e1
Lilleman <jonasss@kth.se>
2021-11-29 13:55:30 +0100

    Created files

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Counter from '../components/Counter'
 
 const Homepage = () => {
     return (
         <div>
             <h1>Covindex</h1>
             <Link to="/counter">Counter</Link>
+            <Counter />
         </div>
     )
 }
