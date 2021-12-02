c77d791af4eb7d97c01d6960963f1426e151d5e3
Edvinn <ealvaeus@kth.se>
2021-12-02 22:21:29 +0100

    bugfix

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,19 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import GlobalStatistics from "../components/GlobalStatistics";
 import GlobalNewsFeed from "../components/newsComponents/GlobalNewsFeed";
 import TwitterFeed from "../components/TwitterFeed";
 import Navbar from "../components/Navbar";
 
 const Homepage = () => {
-  return (
-    <div>
-      <Navbar />
-      <GlobalStatistics />
-      <GlobalNewsFeed />
-      <TwitterFeed />
-    </div>
-  );
+    return (
+        <div>
+            <Navbar />
+            <GlobalStatistics />
+            <GlobalNewsFeed />
+            <TwitterFeed />
+        </div>
+    );
 };
 
 export default Homepage;
