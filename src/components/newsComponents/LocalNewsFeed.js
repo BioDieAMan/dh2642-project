63a6bfb8abb064ed68bf70be49814f5481d5f35f
Edvinn <ealvaeus@kth.se>
2021-12-02 23:13:25 +0100

    added key to news to remove console error

--- src/components/newsComponents/LocalNewsFeed.js
+++ src/components/newsComponents/LocalNewsFeed.js
@@ -17,18 +17,18 @@ const LocalNewsFeed = ({
             {loading ? <div>loading...</div>
                 : data ? < div >
                     {data.map(article =>
-                        <div className="newsCard">
+                        <div className="newsCard" key={article.url}>
                             <a href={article.url}>
                                 <div className="newsContainer">
-                                    {'image' in article?
-                                        <img className="newsImage" src={article.image.thumbnail.contentUrl} />:
+                                    {'image' in article ?
+                                        <img className="newsImage" src={article.image.thumbnail.contentUrl} /> :
                                         <img className="newsImage" src="https://nbhc.ca/sites/default/files/styles/article/public/default_images/news-default-image%402x_0.png?itok=B4jML1jF" />
                                     }
                                     <div className="newsOverlay">
                                         <div className="newsText">
                                             <p>{article.description}</p>
                                             <p className="newsProvider">{article.provider[0].name}</p>
-                                            <p className="newsDate">{article.datePublished.slice(0,10)}</p>
+                                            <p className="newsDate">{article.datePublished.slice(0, 10)}</p>
                                         </div>
                                     </div>
                                 </div>
