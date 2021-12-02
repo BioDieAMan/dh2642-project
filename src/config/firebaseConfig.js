e22f96c4c3078f6b808268b763d4c662cc12f6e5
Edvinn <ealvaeus@kth.se>
2021-12-02 18:37:22 +0100

    firebase setup

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC5LRomlEdaktS_-XPX9-D_eGO4xG--yIY",
+    authDomain: "covindex-dh2642-project.firebaseapp.com",
+    projectId: "covindex-dh2642-project",
+    storageBucket: "covindex-dh2642-project.appspot.com",
+    messagingSenderId: "216944655997",
+    appId: "1:216944655997:web:250c671ebf2557f1defb14"
+};
+
+const app = initializeApp(firebaseConfig);
