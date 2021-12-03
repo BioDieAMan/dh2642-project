31b5705f3de46ba390a28e2092d3660787acec95
Lilleman <jonasss@kth.se>
2021-12-03 19:38:33 +0100

    Comparison Table Component fixed

--- src/components/CountryComparisonTable.js
+++ src/components/CountryComparisonTable.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { connect } from "react-redux";
 import { getCurrentData } from '../redux/actions/countryActions';
+import { TableContainer, Table, TableBody, TableHead, TableRow, TableCell, Paper } from '@mui/material';
 
 const CountryComparisonTable = ({
     selectedCountries,
@@ -10,50 +11,34 @@ const CountryComparisonTable = ({
     
 }) => {
     return (
-        <div>
-            <table>
-                <tr>
-                    <th>Country</th>
-                    <th>Confirmed</th>
-                    <th>Deaths</th>
-                </tr>
-                {selectedCountries.map(scKey => 
-                    <tr>
-                        <td>{listOfCountries[scKey]}</td>
-                        <td>{loadingCurrent ?
-                            <p>loading...</p>:
-                            currentData[scKey] ? 
-                                currentData[scKey].confirmed:
-                                <p>No data!</p>}
-                        </td>
-                        <td>{loadingCurrent ?
-                            <p>loading...</p>:
-                            currentData[scKey] ? 
-                                currentData[scKey].deaths:
-                                <p>No data!</p>}
-                        </td>
-                    </tr>
-                )}
-
-            </table>
-
-
-            {/* // <div className="countryContainer">{Object.keys(countries).map(cKey =>
-            //     <div className={selectedCountries.some(scKey => scKey === cKey) ? "selectedCountryItem":"countryItem"}>
-            //         {watchCountries.some(wcKey => wcKey === cKey)?
-            //             <span className="countryStar" onClick={() => removeWatchCountry(cKey)}>&#9733; </span>:
-            //             <span className="countryStar" onClick={() => addWatchCountry(cKey)}>&#9734; </span>
-            //         }
-
-            //         {selectedCountries.some(scKey => scKey === cKey)?
-            //             <span className="countryText" key={countries[cKey]} value={cKey} onClick={() => removeSelectedCountry(cKey)}>{countries[cKey]}</span>:
-            //             <span className="countryText" key={countries[cKey]} value={cKey} onClick={() => addSelectedCountry(cKey)}>{countries[cKey]}</span>
-            //         }
-            //     </div>
-            //     )}
-            // </div> */}
-
-        </div >
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Country</TableCell>
+                        <TableCell>Confirmed</TableCell>
+                        <TableCell>Confirmed/Capita</TableCell>
+                        <TableCell>Vaccinated</TableCell>
+                        <TableCell>Vaccinated/Capita</TableCell>
+                        <TableCell>Deaths</TableCell>
+                        <TableCell>Deaths/Capita</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {selectedCountries.map(scKey => 
+                        <TableRow>
+                            <TableCell>{listOfCountries[scKey]}</TableCell>
+                            <TableCell>TODO</TableCell>
+                            <TableCell>TODO</TableCell>
+                            <TableCell>TODO</TableCell>
+                            <TableCell>TODO</TableCell>
+                            <TableCell>TODO</TableCell>
+                            <TableCell>TODO</TableCell>
+                        </TableRow>
+                    )}
+                </TableBody>
+            </Table>
+        </TableContainer >
     )
 }
 
