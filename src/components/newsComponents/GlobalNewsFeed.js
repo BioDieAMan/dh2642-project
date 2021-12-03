e865a46517990bdf7fa4633a3b7990e92990e25e
Lilleman <jonasss@kth.se>
2021-12-03 20:56:18 +0100

    NewsComponent Started

--- src/components/newsComponents/GlobalNewsFeed.js
+++ src/components/newsComponents/GlobalNewsFeed.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { connect } from "react-redux";
 import { getGlobalCovidNews } from '../../redux/actions/covidNewsActions';
+import { Typography, Card, CardActions, CardActionArea, CardContent, CardMedia, Button } from "@mui/material";
 
 const GlobalNewsFeed = ({
     data,
@@ -13,7 +14,38 @@ const GlobalNewsFeed = ({
 
     return (
         <div>
-            <h2>Covid News</h2>
+            <Typography variant="h6" align="left" color="textPrimary" gutterBottom>Covid News</Typography>
+
+            {loading?
+                <div>Loading</div>:
+                !data?
+                    <div>No data</div>:
+                    data.map(article =>
+                        <Card className="newsCard">
+                            <CardActionArea>
+                                    <CardMedia
+                                        className="newsImage"
+                                        component="img"
+                                        height="140"
+                                        image={article.image?
+                                            article.image.thumbnail.contentUrl:
+                                            "https://nbhc.ca/sites/default/files/styles/article/public/default_images/news-default-image%402x_0.png?itok=B4jML1jF"
+                                        }
+                                    />
+                            
+                                <CardContent className="newsContent">
+                                    <Typography className="newsTitle" gutterBottom variant="h6" component="div">{article.name}</Typography>
+                                    <Typography className="newsDescription" variant="body2" color="text.secondary">{article.description}</Typography>
+                                    <Typography className="newsProvider" variant="body2" color="text.secondary">{article.provider[0].name}</Typography>
+                                    <Typography className="newsDate" variant="body2" color="text.secondary">{article.datePublished.slice(0, 10)}</Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    )
+            }
+            
+
+            {/* <h2>Covid News</h2>
             {loading ? <div>loading...</div>
                 : data ? < div >
                     {data.map(article =>
@@ -37,7 +69,7 @@ const GlobalNewsFeed = ({
                         </div>
                     )}
                 </div >
-                    : <div>No data</div>}
+                    : <div>No data</div>} */}
         </div>
     )
 }
