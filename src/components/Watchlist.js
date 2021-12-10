27cf95db507e61e3fd14312845b79371d772b4a0
Lilleman <jonasss@kth.se>
2021-12-10 19:22:35 +0100

    Start on watchlist style

--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -2,7 +2,10 @@ import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { getCurrentData, getListOfCountries } from '../redux/actions/countryActions'
-import { CircularProgress } from '@mui/material'
+import { Grid, Card, CardContent, CardActions, Typography, Button, CircularProgress } from '@mui/material'
+import Carousel from 'react-material-ui-carousel';
+import { height } from '@mui/system'
+
 
 const Watchlist = ({
     uid,
@@ -25,21 +28,67 @@ const Watchlist = ({
     }
 
     return (
-        !listOfCountries?
-            <div></div>:
-            !watchlist?
+        <div>
+            {!listOfCountries?
                 <div></div>:
-                <div>
-                    {watchlist.map(country => (
-                        <div>
-                            {listOfCountries[country]}<br />
-                            {/* {loadingCurrent ? <CircularProgress /> : currentData[country].deaths} */}
-                        </div>
-                    ))}
-                    <Link to="/account">Modify watchlist</Link>
-                </div>
+                !watchlist?
+                    <div></div>:
+                    <div>
+                        <Carousel   className='watchlist' stopAutoPlayOnHover={true} interval={5000} animation={"slide"} navButtonsAlwaysVisible={true}
+                                    navButtonsProps={{ style: { backgroundColor: "#6271a3", opacity: 0.2 }}}
+                        >
+                            {watchlist.map(wKey => {
+                                return (
+                                    <Card className='watchlistCard'>
+                                        <CardContent>
+                                            <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
+                                                {listOfCountries[wKey]}
+                                            </Typography>
+
+                                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                                <table>
+                                                    <tr>
+                                                        <td>Confirmed cases: </td>
+                                                        <td>TODO</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Vaccinated: </td>
+                                                        <td>TODO</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Deaths: </td>
+                                                        <td>TODO</td>
+                                                    </tr>
+                                                </table>
+                                            </Typography>
+                                        </CardContent>
+                                        <CardActions>
+                                            <Button size="small">Learn More</Button>
+                                        </CardActions>
+                                    </Card>
+                                )
+                            })}
+                        </Carousel>
+
+                        <Link to="/account">Modify watchlist</Link>
+                    </div>
+
+                    
+                    // <div>
+                    //     {watchlist.map(country => (
+                    //         <div>
+                    //             {listOfCountries[country]}<br />
+                    //             {/* {loadingCurrent ? <CircularProgress /> : currentData[country].deaths} */}
+                    //         </div>
+                    //     ))}
+                    //     <Link to="/account">Modify watchlist</Link>
+                    // </div>
+            }
+        </div>
     )
 }
+
+
 
 const mapStateToProps = (state) => {
     return {
